38d7b41c2887e3653688a6f9be1e2cf033905fbc
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-03-03 16:23:01 +0000

    Adiciona funcao de inserir na avl

--- src/AVLTree.cpp
+++ src/AVLTree.cpp
@@ -4,12 +4,13 @@
 
 AVLTree::AVLTree() 
 {
-    this->setDir(nullptr);
-    this->setEsq(nullptr);
+    this->setDir(NULL);
+    this->setEsq(NULL);
     this->setFBal();
-    this->id = NULL;
+    this->id = -1;
 }
 
+void AVLTree::setID(size_t id) { this->id = id; }
 void AVLTree::setDir(AVLTree* dir) { this->dir = dir; }
 void AVLTree::setEsq(AVLTree* esq) { this->esq = esq; }
 void AVLTree::setFBal()
@@ -18,10 +19,12 @@ void AVLTree::setFBal()
 }
 
 size_t AVLTree::getAltura(AVLTree* no) {
-    if (no == nullptr)
+    if (no == NULL)
         return 0;
     return 1 + fmax(getAltura(this->dir), getAltura(this->esq));
 }
+size_t AVLTree::getID() { return this->id; }
+size_t AVLTree::getFBal() { return this->fbal; }
 AVLTree* AVLTree::getEsq() { return this->esq; }
 AVLTree* AVLTree::getDir() { return this->dir; }
 
@@ -29,11 +32,17 @@ void AVLTree::rotSEsq(AVLTree* noP) {
     AVLTree* noQ = noP->getDir();
     noP->setDir(noQ->getEsq());
     noQ->setEsq(noP);
+
+    noP->setFBal();
+    noQ->setFBal();
 }
 void AVLTree::rotSDir(AVLTree* noP) {
     AVLTree* noQ = noP->getEsq();
     noP->setEsq(noQ->getDir());
     noQ->setDir(noP);
+
+    noP->setFBal();
+    noQ->setFBal();
 }
 void AVLTree::rotDEsq(AVLTree* noP) {
     AVLTree* noQ = noP->getDir();
@@ -42,6 +51,10 @@ void AVLTree::rotDEsq(AVLTree* noP) {
     noQ->setEsq(noR->getDir());
     noR->setEsq(noP);
     noR->setDir(noQ);
+    
+    noP->setFBal();
+    noQ->setFBal();
+    noR->setFBal();
 }
 void AVLTree::rotDDir(AVLTree* noP) {
     AVLTree* noQ = noP->getEsq();
@@ -50,9 +63,45 @@ void AVLTree::rotDDir(AVLTree* noP) {
     noQ->setDir(noR->getEsq());
     noR->setDir(noP);
     noR->setEsq(noQ);
+    
+    noP->setFBal();
+    noQ->setFBal();
+    noR->setFBal();
 }
 
-void AVLTree::insere(size_t id) 
+void AVLTree::insere(AVLTree* no, size_t id) 
 {
-
+    if (no == NULL) {
+        no->setDir(NULL);
+        no->setEsq(NULL);
+        no->setID(id);
+        no->setFBal();
+        return;
+    }
+    else {
+        if (id > no->getID()) {
+            insere(no->getDir(), id);
+        }
+        else if (id < no->getID()) {
+            insere(no->getEsq(), id);
+        }
+        else //no ja existe na arvore
+            return; 
+    }
+    if (no->getFBal() == 2) {
+        if (no->getDir()->getFBal() == -1) {
+            no->rotDEsq(no);
+        } 
+        else {
+            no->rotSEsq(no);
+        } 
+    }
+    else if (no->getFBal() == -2) {
+        if (no->getEsq()->getFBal() == 1) {
+            no->rotDDir(no);
+        }
+        else {
+            no->rotSDir(no);
+        }
+    }
 }
