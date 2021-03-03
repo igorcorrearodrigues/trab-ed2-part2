42ace6d895de4b3cbdf15dd714b0fc655ce274e6
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-03-03 02:08:29 +0000

    adiciona estrutura basica da avl

--- include/AVLTree.hpp
+++ include/AVLTree.hpp
@@ -1,12 +1,28 @@
 #ifndef AVLTREE_HPP
 #define AVLTREE_HPP
 
+#include <cmath>
+
 class AVLTree 
 {
     private:
         size_t id;
+        AVLTree* esq;
+        AVLTree* dir;
+        size_t fbal;
     public:
-        getID();
+        AVLTree();
+
+        void setEsq(AVLTree* esq);
+        void setDir(AVLTree* dir);
+        void setFBal();
+
+        size_t getAltura(AVLTree* no);
+
+        void rotSDir(AVLTree* noP);
+        void rotSEsq(AVLTree* noP);
+
+        void insere(size_t id);
 }
 
 #endif // AVLTREE_HPP
--- src/AVLTree.cpp
+++ src/AVLTree.cpp
@@ -1,2 +1,27 @@
 #include "AVLTree.hpp"
 
+#include <cmath>
+
+AVLTree::AVLTree() 
+{
+    this->setDir(nullptr);
+    this->setEsq(nullptr);
+}
+
+void AVLTree::setDir(AVLTree* dir) { this->dir = dir; }
+void AVLTree::setEsq(AVLTree* esq) { this->esq = esq; }
+void AVLTree::setFBal()
+{
+    this->fbal = this->getAltura(this->dir) - this->getAltura(this->esq);
+}
+
+size_t AVLTree::getAltura(AVLTree* no) {
+    if (no == nullptr)
+        return 0;
+    return 1 + fmax(getAltura(this->dir), getAltura(this->esq));
+}
+
+void AVLTree::insere(size_t id) 
+{
+
+}
