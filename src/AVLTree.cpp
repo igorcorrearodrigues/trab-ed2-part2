961a7483b7734f3f6590408e6424bb35196840b4
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-03-03 02:55:01 +0000

    implementado rotacoes da avl

--- src/AVLTree.cpp
+++ src/AVLTree.cpp
@@ -22,6 +22,35 @@ size_t AVLTree::getAltura(AVLTree* no) {
         return 0;
     return 1 + fmax(getAltura(this->dir), getAltura(this->esq));
 }
+AVLTree* AVLTree::getEsq() { return this->esq; }
+AVLTree* AVLTree::getDir() { return this->dir; }
+
+void AVLTree::rotSEsq(AVLTree* noP) {
+    AVLTree* noQ = noP->getDir();
+    noP->setDir(noQ->getEsq());
+    noQ->setEsq(noP);
+}
+void AVLTree::rotSDir(AVLTree* noP) {
+    AVLTree* noQ = noP->getEsq();
+    noP->setEsq(noQ->getDir());
+    noQ->setDir(noP);
+}
+void AVLTree::rotDEsq(AVLTree* noP) {
+    AVLTree* noQ = noP->getDir();
+    AVLTree* noR = noQ->getEsq();
+    noP->setDir(noR->getEsq());
+    noQ->setEsq(noR->getDir());
+    noR->setEsq(noP);
+    noR->setDir(noQ);
+}
+void AVLTree::rotDDir(AVLTree* noP) {
+    AVLTree* noQ = noP->getEsq();
+    AVLTree* noR = noQ->getDir();
+    noP->setEsq(noR->getDir());
+    noQ->setDir(noR->getEsq());
+    noR->setDir(noP);
+    noR->setEsq(noQ);
+}
 
 void AVLTree::insere(size_t id) 
 {
