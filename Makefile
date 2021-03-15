ff9bc56d417e98316eece5667557b1d08c5ad013
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2021-03-15 15:19:42 -0300

    Adaptando o módulo ao novo modelo das árvores

--- Makefile
+++ Makefile
@@ -3,46 +3,15 @@ CC_FLAGS=-W -Wall -pedantic -std=c++11
 INC_DIR=./include
 SRC_DIR=./src
 
-MAIN_SRC=main.cpp \
-				 $(SRC_DIR)/Cidade.cpp \
-				 $(SRC_DIR)/CodigoDaCidade.cpp \
-				 $(SRC_DIR)/ArvB.cpp \
-				 $(SRC_DIR)/NoB.cpp \
-				 $(SRC_DIR)/AVLTree.cpp \
-				 $(SRC_DIR)/Registro.cpp   \
-				 $(SRC_DIR)/HashTable.cpp  \
-				 $(SRC_DIR)/Quadtree.cpp   \
-				 $(SRC_DIR)/string_tok.cpp
+CPP_SOURCE=$(wildcard $(SRC_DIR)/*.cpp)
 
-MAIN_HPP=$(INC_DIR)/Cidade.hpp    \
-				 $(INC_DIR)/Registro.hpp  \
-				 $(INC_DIR)/CodigoDaCidade.hpp  \
-				 $(INC_DIR)/ArvB.hpp  \
-				 $(INC_DIR)/AVLTree.hpp  \
-				 $(INC_DIR)/NoB.hpp  \
-				 $(INC_DIR)/HashTable.hpp \
-				 $(INC_DIR)/Quadtree.hpp 
+HPP_SOURCE=$(wildcard $(INC_DIR)/*.hpp)
 
+MAIN_SRC=main.cpp $(CPP_SOURCE)
+MAIN_HPP=$(HPP_SOURCE)
 
-MODULO_HPP=$(INC_DIR)/Quadtree.hpp \
-					 $(INC_DIR)/HashTable.hpp \
-					 $(INC_DIR)/NoB.hpp    \
-					 $(INC_DIR)/ArvB.hpp    \
-					 $(INC_DIR)/AVLTree.hpp \
-					 $(INC_DIR)/AVLNode.hpp \
-					 $(INC_DIR)/Cidade.hpp \
-					 $(INC_DIR)/Registro.hpp \
-					 $(INC_DIR)/utils.hpp
-
-MODULO_SRC=modulo_de_teste.cpp      \
-					 $(SRC_DIR)/AVLTree.cpp   \
-					 $(SRC_DIR)/NoB.cpp      \
-					 $(SRC_DIR)/ArvB.cpp      \
-					 $(SRC_DIR)/Cidade.cpp    \
-					 $(SRC_DIR)/HashTable.cpp \
-					 $(SRC_DIR)/Quadtree.cpp  \
-					 $(SRC_DIR)/Registro.cpp  \
-					 $(SRC_DIR)/string_tok.cpp 
+MODULO_SRC=modulo_de_teste.cpp $(CPP_SOURCE)
+MODULO_HPP=$(HPP_SOURCE)
 
 
 build: main
--- modulo_de_teste.cpp
+++ modulo_de_teste.cpp
@@ -12,6 +12,9 @@ using namespace std;
 
 #define LIMITE_N 20
 
+std::string arquivoRegistros = "/brazil_covid19_cities_processado.csv";
+std::string arquivoCidades = "/brazil_cities_coordinates.csv";
+
 void insereNaQuadtree(Quadtree& q, std::ostream& out, size_t n);
 void insereNaTabelaHash(HashTable& t, std::ostream& out, size_t n);
 void insereNaArvoreAvl(AVLTree& arv, std::ostream& out, size_t n);
@@ -22,7 +25,7 @@ void insereNaArvoreB(ArvB& arv, std::ostream& out, size_t n);
     {                                                        \
         _estrutura est;                                      \
         if (n > LIMITE_N) {                                  \
-            ofstream arquivoSaida(#_estrutura ".saida.txt"); \
+            ofstream arquivoSaida(#_estrutura ".txt"); \
             _function_name(est, arquivoSaida, n);            \
         } else {                                             \
             _function_name(est, std::cout, n);               \
@@ -31,28 +34,47 @@ void insereNaArvoreB(ArvB& arv, std::ostream& out, size_t n);
 
 DECLARA_INSERE_FUNCTION(insereNaQuadtree, Quadtree)
 DECLARA_INSERE_FUNCTION(insereNaTabelaHash, HashTable)
-DECLARA_INSERE_FUNCTION(insereNaArvoreAvl, AVLTree)
+
+void insereNaArvoreAvl(size_t n)
+{
+    HashTable ht;
+    AVLTree arv(&ht);
+
+    if (n > LIMITE_N) {
+        ofstream arquivoSaida("ArvB20.txt");
+        insereNaTabelaHash(ht, arquivoSaida, n);
+        insereNaArvoreAvl(arv, arquivoSaida, n);
+    } else {
+        insereNaArvoreAvl(arv, std::cout, n);
+    }
+}
 
 void insereNaArvoreB20(size_t n)
 {
-    ArvB arv(20);
+    HashTable ht;
+    ArvB arv(20, &ht);
 
     if (n > LIMITE_N) {
-        ofstream arquivoSaida("ArvB20.saida.txt");
+        ofstream arquivoSaida("ArvB20.txt");
+        insereNaTabelaHash(ht, arquivoSaida, n);
         insereNaArvoreB(arv, arquivoSaida, n);
     } else {
+        insereNaTabelaHash(ht, std::cout, n);
         insereNaArvoreB(arv, std::cout, n);
     }
 }
 
 void insereNaArvoreB200(size_t n)
 {
-    ArvB arv(200);
+    HashTable ht;
+    ArvB arv(200, &ht);
 
     if (n > LIMITE_N) {
-        ofstream arquivoSaida("ArvB200.saida.txt");
+        ofstream arquivoSaida("ArvB200.txt");
+        insereNaTabelaHash(ht, arquivoSaida, n);
         insereNaArvoreB(arv, arquivoSaida, n);
     } else {
+        insereNaTabelaHash(ht, std::cout, n);
         insereNaArvoreB(arv, std::cout, n);
     }
 }
@@ -88,10 +110,20 @@ MenuOpcao *getOpcao(unsigned n)
     return validOption(n) ? &opcoes[n - 1] : nullptr;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     unsigned opcao;
+    extern std::string arquivoCidades;
+    extern std::string arquivoRegistros;
     extern MenuOpcao opcoes[];
+
+    if (argc != 2) {
+        std::cerr << "Uso: " << *argv << " DIR_DOS_ARQUIVOS_CSV\n";
+        return 1;
+    }
+
+    arquivoCidades = argv[1] + arquivoCidades;
+    arquivoRegistros = argv[1] + arquivoRegistros;
 
     do
     {
@@ -135,11 +167,11 @@ int main()
 
 vector<Cidade> getNCidades(size_t n)
 {
-    const char filename[] = "./datasets/brazil_cities_coordinates.csv";
-    std::ifstream in(filename);
+    extern std::string arquivoCidades;
+    std::ifstream in(arquivoCidades);
 
     if (!in.is_open()) {
-        std::cerr << "Falha ao abrir o arquivo `" << filename << "`\n";
+        std::cerr << "Falha ao abrir o arquivo `" << arquivoCidades << "`\n";
         return std::vector<Cidade>{};
     }
 
@@ -155,11 +187,11 @@ vector<Cidade> getNCidades(size_t n)
 
 vector<Registro> getNRegistros(size_t n)
 {
-    const char filename[] = "./datasets/brazil_covid19_cities_processado.csv";
-    std::ifstream in(filename);
+    extern std::string arquivoRegistros;
+    std::ifstream in(arquivoRegistros);
 
     if (!in.is_open()) {
-        std::cerr << "Falha ao abrir o arquivo `" << filename << "`\n";
+        std::cerr << "Falha ao abrir o arquivo `" << arquivoRegistros << "`\n";
         return std::vector<Registro>{};
     }
 
